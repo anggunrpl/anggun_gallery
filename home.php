9e46808431e59fa85b45d6571cdb30fb50d29ed2
anggunrpl <160198006+anggunrpl@users.noreply.github.com>
2024-04-17 20:04:52 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['userid'])){
+        header("location:login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome To Website Gallery Foto</h1>
+    </header>
+    <nav>
+</head>
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="album.php">Album</a></li>
+        <li><a href="foto.php">Foto</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+</html>
